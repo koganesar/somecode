2a648e9257cfb702daea4f793eb07be30247651b
koganesar <garaeva.alsu.1998@mail.ru>
2021-04-07 14:51:08 +0300

    Main structure of project

---
+++ iskluchenie/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace Exceptions
+{
+    class ExeptionCheking
+    {
+        public int a;
+
+        public ExeptionCheking(int a)
+        {
+            this.a = a;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SimpleMethod(5, 0, new ExeptionCheking(5), true);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                SimpleMethod(5, 4, null, true);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                SimpleMethod(6, 7, new ExeptionCheking(5), true);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                SimpleMethod(5, 4, new ExeptionCheking(5), false);
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static double SimpleMethod(int x, int y, ExeptionCheking exeptionCheking, bool test)
+        {
+            var array = new int[x];
+            _ = array[y];
+
+            if (exeptionCheking is null)
+            {
+                throw new ArgumentNullException("ExeptionCheking null");
+            }
+
+            bool flag = true;
+
+            _ = !test ? Convert.ToChar(flag) : 'a';
+            _ = exeptionCheking.a;
+
+            return x / y;
+        }
+    }
+}
